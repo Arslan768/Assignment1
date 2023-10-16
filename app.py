d61c0d28651c4897d3e231aac7a65e0e08036e41
Arslan Ahmed <i202444@nu.edu.pk>
2023-10-16 07:50:53 +0500

    Random Commit

--- app.py
+++ app.py
@@ -3,21 +3,29 @@ import joblib
 
 app = Flask(__name__)
 
-# Load the pre-trained Iris model
 model = joblib.load('Model/model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     try:
         data = request.get_json()
+        if data is None:
+            return jsonify({'error': 'Invalid JSON data'}), 400
+
+        required_keys = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+        for key in required_keys:
+            if key not in data:
+                return jsonify({'error': f'Missing key: {key}'}), 400
+
         sepal_length = data['sepal_length']
         sepal_width = data['sepal_width']
         petal_length = data['petal_length']
         petal_width = data['petal_width']
+
         prediction = model.predict([[sepal_length, sepal_width, petal_length, petal_width]])[0]
-        return jsonify({' PREDICTION : ': prediction})
+        return jsonify({'PREDICTION': prediction}), 200
     except Exception as e:
-        return jsonify({'error': str(e)})
+        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
